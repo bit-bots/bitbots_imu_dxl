477a49ab5a6d6384202128e67a7185bfd391c559
Jasper Güldenstein <5guelden@informatik.uni-hamburg.de>
2020-08-10 17:06:02 +0200

    started fast slave

--- firmware/esp32_imu/esp32_imu.ino
+++ firmware/esp32_imu/esp32_imu.ino
@@ -1,10 +1,10 @@
-#include <MPU9250.h>
+#include <MPU9250.h> // modified version required since we use mpu6500
 #include <Dynamixel2Arduino.h>
 #include <FastLED.h>
 #include <array>
 #include "complementary_filter.h"
 #include <Preferences.h>
-
+#include "fast_slave.h"
 
 // define two tasks for reading the dxl bus and doing other work
 void TaskDXL( void *pvParameters );
@@ -12,7 +12,8 @@ void TaskWorker( void *pvParameters );
 
 TaskHandle_t th_dxl,th_worker;
 
-Preferences prefs;
+Preferences dxl_prefs;
+Preferences imu_prefs;
 
 /*---------------------- DXL defines and variables ---------------------*/
 
@@ -22,20 +23,20 @@ Preferences prefs;
 #define DEFAULT_ID 241
 #define DEFAULT_BAUD 4 //2mbaud
 
-DYNAMIXEL::SerialPortHandler dxl_port(Serial, DXL_DIR_PIN);
-DYNAMIXEL::Slave dxl(dxl_port, DXL_MODEL_NUM);
+uart_t* uart;
+DYNAMIXEL::FastSlave dxl(NULL, DXL_MODEL_NUM);
 
 #define ADDR_CONTROL_ITEM_BAUD 8
 
 #define ADDR_CONTROL_ITEM_LED0_R 10
 #define ADDR_CONTROL_ITEM_LED0_G 11
 #define ADDR_CONTROL_ITEM_LED0_B 12
-#define ADDR_CONTROL_ITEM_LED1_R 13
-#define ADDR_CONTROL_ITEM_LED1_G 14
-#define ADDR_CONTROL_ITEM_LED1_B 15
-#define ADDR_CONTROL_ITEM_LED2_R 16
-#define ADDR_CONTROL_ITEM_LED2_G 17
-#define ADDR_CONTROL_ITEM_LED2_B 18
+#define ADDR_CONTROL_ITEM_LED1_R 14
+#define ADDR_CONTROL_ITEM_LED1_G 15
+#define ADDR_CONTROL_ITEM_LED1_B 16
+#define ADDR_CONTROL_ITEM_LED2_R 18
+#define ADDR_CONTROL_ITEM_LED2_G 19
+#define ADDR_CONTROL_ITEM_LED2_B 20
 
 #define ADDR_CONTROL_ITEM_GYRO_X 36
 #define ADDR_CONTROL_ITEM_GYRO_Y 40
@@ -55,7 +56,14 @@ DYNAMIXEL::Slave dxl(dxl_port, DXL_MODEL_NUM);
 #define ADDR_CONTROL_ITEM_GYRO_RANGE 102
 #define ADDR_CONTROL_ITEM_ACCEL_RANGE 103
 #define ADDR_CONTROL_ITEM_CALIBRATE_GYRO 104
-//TODO bias gain and alpha
+#define ADDR_CONTROL_ITEM_RESET_GYRO_CALIBRATION 105
+#define ADDR_CONTROL_ITEM_CALIBRATE_ACCEL 104
+
+#define ADDR_CONTROL_ITEM_DO_ADAPTIVE_GAIN 108
+#define ADDR_CONTROL_ITEM_DO_BIAS_ESTIMATION 109
+#define ADDR_CONTROL_ITEM_ACCEL_GAIN 110
+#define ADDR_CONTROL_ITEM_BIAS_ALPHA 114
+
 
 
 // id and baud are stored using preferences for persistance between resets of the chip
@@ -79,12 +87,17 @@ float gyro[3] = {0,0,0};
 float accel[3] = {0,0,0};
 float quat[4] = {0,0,0,0};
 
-uint8_t gyro_range, accel_range, calibrate_gyro;
+uint8_t gyro_range, accel_range;
 
-#define IMU_GAIN_ACCEL 0.04
-#define IMU_DO_ADAPTIVE_GAIN false
-#define IMU_DO_BIAS_ESTIMATION true
-#define IMU_BIAS_ALPHA 0.01
+#define IMU_GAIN_ACCEL_DEFAULT 0.04
+#define IMU_DO_ADAPTIVE_GAIN_DEFAULT false
+#define IMU_DO_BIAS_ESTIMATION_DEFAULT true
+#define IMU_BIAS_ALPHA_DEFAULT 0.01
+
+bool calibrate_gyro = false;
+bool reset_gyro_calibration = false;
+bool do_adaptive_gain, do_bias_estimation;
+float accel_gain, bias_alpha;
 
 imu_tools::ComplementaryFilter filter_;
 float dt = 0; // time delta between readings
@@ -112,6 +125,7 @@ void setup() {
   leds[1] = CRGB::Black;
   leds[2] = CRGB::Black;
   FastLED.show();
+  FastLED.setBrightness(128);
   
   disableCore0WDT(); // required since we dont want FreeRTOS to slow down our reading if the Wachdogtimer (WTD) fires
   disableCore1WDT();
@@ -164,54 +178,60 @@ void write_callback_func(uint16_t item_addr, uint8_t &dxl_err_code, void* arg)
   (void)dxl_err_code, (void)arg;
   if (item_addr == ADDR_CONTROL_ITEM_BAUD)
   {
-    prefs.putUChar("baud", baud);
+    dxl_prefs.putUChar("baud", baud);
     ESP.restart(); // restart whole chip since restarting serial port crashes esp
   } 
   else if(item_addr == ADDR_CONTROL_ITEM_GYRO_RANGE)
   {
-    MPU9250::GyroRange range;
-    switch(gyro_range)
-    {
-      case 0:
-        range = MPU9250::GYRO_RANGE_250DPS;
-        break;
-      case 1:
-        range = MPU9250::GYRO_RANGE_500DPS;
-        break;
-      case 2:
-        range = MPU9250::GYRO_RANGE_1000DPS;
-        break;
-      case 3:
-        range = MPU9250::GYRO_RANGE_2000DPS;
-        break;
-      default:
-        range = GYRO_RANGE_DEFAULT;
-    }
-    IMU.setGyroRange(range);
+    setGyroRange(gyro_range);
+    imu_prefs.putUChar("gyro_range", gyro_range);
+
   }
   else if(item_addr == ADDR_CONTROL_ITEM_ACCEL_RANGE)
   {
-    MPU9250::AccelRange range;
-    switch(accel_range)
-    {
-      case 0:
-        range = MPU9250::ACCEL_RANGE_2G;
-        break;
-      case 1:
-        range = MPU9250::ACCEL_RANGE_4G;
-        break;
-      case 2:
-        range = MPU9250::ACCEL_RANGE_8G;
-        break;
-      case 3:
-        range = MPU9250::ACCEL_RANGE_16G;
-        break;
-      default:
-        range = ACCEL_RANGE_DEFAULT;
-    }
-    IMU.setAccelRange(range);
+    setAccelRange(accel_range);
+    imu_prefs.putUChar("accel_range", accel_range);
   }
-  else if(item_addr >= ADDR_CONTROL_ITEM_LED0_R && item_addr <= ADDR_CONTROL_ITEM_LED2_B) //TODO LEDS
+  else if(item_addr == ADDR_CONTROL_ITEM_CALIBRATE_GYRO)
+  {
+    if(calibrate_gyro)
+    {
+      IMU.calibrateGyro();
+      FastLED.show();
+      calibrate_gyro = false;
+    }
+  }
+  else if(item_addr == ADDR_CONTROL_ITEM_RESET_GYRO_CALIBRATION)
+  {
+    if(reset_gyro_calibration)
+    {
+      IMU.setGyroBiasX_rads(0);
+      IMU.setGyroBiasY_rads(0);
+      IMU.setGyroBiasZ_rads(0);
+      reset_gyro_calibration = 0;
+    }
+  }
+  else if(item_addr == ADDR_CONTROL_ITEM_DO_ADAPTIVE_GAIN)
+  {
+    imu_prefs.putUChar("adaptive_gain", do_adaptive_gain);
+    filter_.setDoAdaptiveGain(do_adaptive_gain);
+  }
+  else if(item_addr == ADDR_CONTROL_ITEM_DO_BIAS_ESTIMATION)
+  {
+    imu_prefs.putUChar("bias_estimation", do_bias_estimation);
+    filter_.setDoBiasEstimation(do_bias_estimation);
+  }
+  else if(item_addr == ADDR_CONTROL_ITEM_ACCEL_GAIN)
+  {
+    imu_prefs.putFloat("gain_accel", IMU_GAIN_ACCEL_DEFAULT);
+    filter_.setGainAcc(accel_gain);
+  }
+  else if(item_addr == ADDR_CONTROL_ITEM_BIAS_ALPHA)
+  {
+    imu_prefs.putFloat("bias_alpha", bias_alpha);
+    filter_.setBiasAlpha(bias_alpha);
+  }
+  else if(item_addr >= ADDR_CONTROL_ITEM_LED0_R && item_addr <= ADDR_CONTROL_ITEM_LED2_B)
   {
     FastLED.show();
   }
@@ -220,18 +240,17 @@ void write_callback_func(uint16_t item_addr, uint8_t &dxl_err_code, void* arg)
 void TaskDXL(void *pvParameters) 
 {
   (void) pvParameters;
-  
-  prefs.begin("dxl");
-  if(!prefs.getUChar("init")) // check if prefs are initialized
-  {
-    prefs.putUChar("id", DEFAULT_ID);
-    prefs.putUChar("baud", DEFAULT_BAUD);
-    prefs.putUChar("init",1); // set initialized
-  }
-  id = prefs.getUChar("id");
-  baud = prefs.getUChar("baud");
 
-  dxl_port.begin(dxl_to_real_baud(baud));
+  dxl_prefs.begin("dxl");
+  if(!dxl_prefs.getUChar("init")) // check if prefs are initialized
+  {
+    dxl_prefs.putUChar("id", DEFAULT_ID);
+    dxl_prefs.putUChar("baud", DEFAULT_BAUD);
+    dxl_prefs.putUChar("init",1); // set initialized
+  }
+  id = dxl_prefs.getUChar("id");
+  baud = dxl_prefs.getUChar("baud");
+
   
   dxl.setPortProtocolVersion(DXL_PROTOCOL_VER_2_0);
   dxl.setFirmwareVersion(1);
@@ -268,26 +287,86 @@ void TaskDXL(void *pvParameters)
   dxl.addControlItem(ADDR_CONTROL_ITEM_GYRO_RANGE, gyro_range);
   dxl.addControlItem(ADDR_CONTROL_ITEM_ACCEL_RANGE, accel_range);
 
-  //todo bias gain...
+  
+  dxl.addControlItem(ADDR_CONTROL_ITEM_CALIBRATE_GYRO, calibrate_gyro);
+  dxl.addControlItem(ADDR_CONTROL_ITEM_RESET_GYRO_CALIBRATION, reset_gyro_calibration);
+
+  dxl.addControlItem(ADDR_CONTROL_ITEM_DO_ADAPTIVE_GAIN, do_adaptive_gain);
+  dxl.addControlItem(ADDR_CONTROL_ITEM_DO_BIAS_ESTIMATION, do_bias_estimation);
+  dxl.addControlItem(ADDR_CONTROL_ITEM_ACCEL_GAIN, accel_gain);
+  dxl.addControlItem(ADDR_CONTROL_ITEM_BIAS_ALPHA, bias_alpha);
+
   
   dxl.setWriteCallbackFunc(write_callback_func);
   
+  // init uart 0, given baud, 8bits 1stop no parity, pin 3 and 1, 256 buffer, no inversion
+  uart = uartBegin(0, dxl_to_real_baud(baud), SERIAL_8N1, 3, 1,  256, false);
+  // disable all interrupts
+  uart->dev->conf1.rx_tout_en = 0;
+  uart->dev->int_ena.rxfifo_full = 0;
+  uart->dev->int_ena.frm_err = 0;
+  uart->dev->int_ena.rxfifo_tout = 0;
+  
   for (;;)
   {
-    dxl.processPacket();
-    
-    if(dxl.getID() != id) // since we cant add the id as a control item, we need to check if it has been updated manually
-    {
-      id = dxl.getID();
-      prefs.putUChar("id", id);
+    if(dxl.processPacket(uart)){
+      if(dxl.getID() != id) // since we cant add the id as a control item, we need to check if it has been updated manually
+      {
+        id = dxl.getID();
+        dxl_prefs.putUChar("id", id);
+      }
     }
   }
 }
 
 /*---------------------- IMU ---------------------*/
-
-
+void setAccelRange(uint8_t range)
+{
+    MPU9250::AccelRange mpu_range;
+    switch(range)
+    {
+      case 0:
+        mpu_range = MPU9250::ACCEL_RANGE_2G;
+        break;
+      case 1:
+        mpu_range = MPU9250::ACCEL_RANGE_4G;
+        break;
+      case 2:
+        mpu_range = MPU9250::ACCEL_RANGE_8G;
+        break;
+      case 3:
+        mpu_range = MPU9250::ACCEL_RANGE_16G;
+        break;
+      default:
+        mpu_range = ACCEL_RANGE_DEFAULT;
+    }
+    IMU.setAccelRange(mpu_range);
+}
+void setGyroRange(uint8_t range)
+{
+    MPU9250::GyroRange mpu_range;
+    switch(range)
+    {
+      case 0:
+        mpu_range = MPU9250::GYRO_RANGE_250DPS;
+        break;
+      case 1:
+        mpu_range = MPU9250::GYRO_RANGE_500DPS;
+        break;
+      case 2:
+        mpu_range = MPU9250::GYRO_RANGE_1000DPS;
+        break;
+      case 3:
+        mpu_range = MPU9250::GYRO_RANGE_2000DPS;
+        break;
+      default:
+        mpu_range = GYRO_RANGE_DEFAULT;
+    }
+    IMU.setGyroRange(mpu_range);
+}
 void getIMU(){
+    if(calibrate_gyro)
+      return;
     
     buttons[0] = !digitalRead(BUTTON0_PIN);
     buttons[1] = !digitalRead(BUTTON1_PIN);
@@ -322,9 +401,9 @@ void getIMU(){
     
     if (filter_.getDoBiasEstimation())
     {
-      accel[0] -= filter_.getAngularVelocityBiasX();
-      accel[1] -= filter_.getAngularVelocityBiasY();
-      accel[2] -= filter_.getAngularVelocityBiasZ();
+      gyro[0] -= filter_.getAngularVelocityBiasX();
+      gyro[1] -= filter_.getAngularVelocityBiasY();
+      gyro[2] -= filter_.getAngularVelocityBiasZ();
     }
 }
 
@@ -332,19 +411,39 @@ void TaskWorker(void *pvParameters)
 {
   (void) pvParameters;
 
-  IMU.begin();
-  IMU.setSrd(SAMPLE_RATE_DIVIDER);
-  IMU.setAccelRange(ACCEL_RANGE_DEFAULT);
-  IMU.setGyroRange(GYRO_RANGE_DEFAULT);
-  IMU.enableDataReadyInterrupt();
-  
-  filter_.setDoAdaptiveGain(IMU_DO_ADAPTIVE_GAIN);
-  filter_.setGainAcc(IMU_GAIN_ACCEL);
-  filter_.setDoBiasEstimation(IMU_DO_BIAS_ESTIMATION);
-  if (IMU_DO_BIAS_ESTIMATION)
+  imu_prefs.begin("imu");
+
+  if(!imu_prefs.getUChar("init")) // check if prefs are initialized
   {
-    filter_.setBiasAlpha(IMU_BIAS_ALPHA);
+    imu_prefs.putUChar("accel_range", ACCEL_RANGE_DEFAULT);
+    imu_prefs.putUChar("gyro_range", GYRO_RANGE_DEFAULT);
+    
+    imu_prefs.putUChar("adaptive_gain", IMU_DO_ADAPTIVE_GAIN_DEFAULT);
+    imu_prefs.putUChar("bias_estimation", IMU_DO_BIAS_ESTIMATION_DEFAULT);
+    imu_prefs.putFloat("gain_accel", IMU_GAIN_ACCEL_DEFAULT);
+    imu_prefs.putFloat("bias_alpha", IMU_BIAS_ALPHA_DEFAULT);
+    
+    imu_prefs.putUChar("init",1); // set initialized
   }
+  delay(1000);
+  int status = IMU.begin();
+  Serial.print("status: ");
+  Serial.println(status);
+  IMU.setSrd(50);
+  setGyroRange(imu_prefs.getUChar("gyro_range"));
+  setAccelRange(imu_prefs.getUChar("accel_range"));
+  IMU.enableDataReadyInterrupt();
+
+  do_adaptive_gain = imu_prefs.getUChar("adaptive_gain");
+  accel_gain = imu_prefs.getFloat("gain_accel");
+  do_bias_estimation = imu_prefs.getUChar("do_bias_estimation");
+  bias_alpha = imu_prefs.getFloat("bias_alpha");
+  
+  filter_.setDoAdaptiveGain(do_adaptive_gain);
+  filter_.setGainAcc(accel_gain);
+  filter_.setDoBiasEstimation(do_bias_estimation);
+  filter_.setBiasAlpha(bias_alpha);
+
   pinMode(BUTTON0_PIN,INPUT_PULLUP);
   pinMode(BUTTON1_PIN,INPUT_PULLUP);
   pinMode(BUTTON2_PIN,INPUT_PULLUP);
