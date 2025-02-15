184d41ea96c4919bdcf68592ba2c6f81962c79d0
Florian Vahl <7vahl@informatik.uni-hamburg.de>
2025-02-15 11:57:19 +0100

    Add storage and multi core

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -4,19 +4,26 @@
 mod transport;
 
 use crate::transport::DynamixelSerial;
+use core::cell::RefCell;
+use core::ptr::addr_of_mut;
 use core::time::Duration;
+use critical_section::Mutex;
 use defmt::Debug2Format;
 use dynamixel2::{Device, Instructions, ReadError, SerialPort, TransferError};
+use embedded_storage::{ReadStorage, Storage};
 use esp_backtrace as _;
 use esp_hal::clock::CpuClock;
-use esp_hal::main;
+use esp_hal::cpu_control::{CpuControl, Stack};
+use esp_hal::gpio::{Level, Output};
 use esp_hal::uart::{Config as UartConfig, Uart};
+use esp_hal::{delay::Delay, main};
+use esp_storage::FlashStorage;
 use log::{error, info, warn};
-
-extern crate alloc;
 
 const BUS_BOUDRATE: u32 = 115200;
 const ID: u8 = 1;
+
+static mut APP_CORE_STACK: Stack<8192> = Stack::new();
 
 #[main]
 fn main() -> ! {
@@ -26,24 +33,58 @@ fn main() -> ! {
     let peripherals = esp_hal::init(config);
 
     esp_println::logger::init_logger_from_env();
-    esp_alloc::heap_allocator!(72 * 1024);
+    info!("Starting up");
+    let common_state = Mutex::new(RefCell::new(0)); // TODO replace with actual struct
 
-    let (rx_pin, tx_pin) = (peripherals.GPIO16, peripherals.GPIO17);
+    // Store simple data in flash
+    info!("Setting up persistent storage");
+    let mut storage = FlashStorage::new();
 
+    // Read data from flash from the previous run
+    let mut data: [u8; 5] = [0; 5];
+    storage
+        .read(0, &mut data)
+        .expect("Failed to read data from flash");
+    info!("Data from flash: {:?}", data);
+
+    let data: [u8; 5] = [1, 2, 3, 4, 5];
+    storage
+        .write(0, &data)
+        .expect("Failed to write data to flash");
+
+    // Setup UART communication
+    info!("Setting up UART communication");
     let uart = Uart::new(
         peripherals.UART2,
         UartConfig::default().with_baudrate(BUS_BOUDRATE),
     )
     .expect("Failed to initialize UART controller")
-    .with_rx(rx_pin)
-    .with_tx(tx_pin);
+    .with_rx(peripherals.GPIO21)
+    .with_tx(peripherals.GPIO23);
+    let mut dir_pin = Output::new(peripherals.GPIO22, Level::Low);
 
-    let transport = DynamixelSerial::new(uart, BUS_BOUDRATE);
+    // Setup the transport layer for the dynamixel communication
+    info!("Setting up dynamixel communication layer");
+    let transport = DynamixelSerial::new(uart, BUS_BOUDRATE, &mut dir_pin);
 
-    device_loop(transport);
+    info!("Setting up secondary core");
+    let mut cpu_control = CpuControl::new(peripherals.CPU_CTRL);
+    let _guard = cpu_control
+        .start_app_core(unsafe { &mut *addr_of_mut!(APP_CORE_STACK) }, || {
+            device_loop(transport, &common_state);
+        })
+        .unwrap();
+
+    info!("Entering main loop on primary core");
+    let delay = Delay::new();
+    loop {
+        // do something
+        delay.delay_millis(1000);
+        warn!("Main loop ran once");
+    }
 }
 
-fn device_loop(transport: DynamixelSerial) -> ! {
+fn device_loop(transport: DynamixelSerial, common_state: &Mutex<RefCell<u32>>) -> ! {
     let mut device = Device::with_buffers(transport, [0; 200], [0; 200])
         .expect("Failed to initialize dynamixel device");
     loop {
--- firmware/src/transport.rs
+++ firmware/src/transport.rs
@@ -3,6 +3,7 @@ use dynamixel2::SerialPort;
 use embedded_io::Write;
 use esp_hal::{
     delay::Delay,
+    gpio::Output,
     time::{now, ExtU64, Instant},
     uart::{Error as UartError, Uart},
     Blocking,
@@ -18,11 +19,19 @@ pub enum Error {
 pub struct DynamixelSerial<'d> {
     serial: Uart<'d, Blocking>,
     baud_rate: u32,
+    dir: &'d mut Output<'d>,
 }
 
 impl<'d> DynamixelSerial<'d> {
-    pub fn new(serial: Uart<'d, Blocking>, baud_rate: u32) -> Self {
-        Self { serial, baud_rate }
+    pub fn new(serial: Uart<'d, Blocking>, baud_rate: u32, dir: &'d mut Output<'d>) -> Self {
+        // Set direction pin to low just to be sure
+        dir.set_low();
+        // Initialize the struct
+        Self {
+            serial,
+            baud_rate,
+            dir,
+        }
     }
 }
 
@@ -59,10 +68,13 @@ impl<'d> SerialPort for DynamixelSerial<'d> {
     }
 
     fn write_all(&mut self, buffer: &[u8]) -> Result<(), Self::Error> {
+        self.dir.set_high();
         self.serial
             .write_all(buffer)
             .map_err(Error::UartWriteError)?;
-        Write::flush(&mut self.serial).map_err(Error::UartWriteError)
+        Write::flush(&mut self.serial).map_err(Error::UartWriteError)?;
+        self.dir.set_low();
+        Ok(())
     }
 
     fn make_deadline(&self, timeout: Duration) -> Self::Instant {
